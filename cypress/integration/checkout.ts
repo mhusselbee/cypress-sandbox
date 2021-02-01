99c284e52721e96d0cf3c266159f0f1989179646
Mateo Husselbee <mhusselbee@cbtnuggets.com>
2021-01-31 16:40:24 -0800

    Waffling

---
+++ cypress/integration/checkout.ts
@@ -0,0 +1,27 @@
+describe('A customer using the checkout workflow', () => {
+    let customerData: CustomerData[];
+
+    it('can fill in their shipping information', () => {
+        cy.fixture('customer-fixtures.json')
+            .as('testData')
+            .then(data => {
+                console.log(data.customers);
+                customerData = data.customers;
+                cy.visit('http://localhost:3000');
+                //@ts-ignore
+                cy.fillShipping(customerData[0]);
+                cy.get('.makeStyles-buttons-5 > .MuiButtonBase-root').click();
+            });
+    });
+
+    it('can fill in their payment information', () => {
+        //@ts-ignore
+        cy.fillPayment(customerData[0]);
+        cy.get('.MuiButton-contained').click();
+    });
+
+    it('can review and confirm their order', () => {
+        cy.get('.MuiTypography-body2.MuiTypography-alignCenter').scrollIntoView();
+        cy.get('.MuiButton-contained').click();
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,27 +1,19 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-Cypress.Commands.add('example', () => {
-    cy.visit('https://www.example.com');
+Cypress.Commands.add('fillShipping', (customerData, fail = false, saveAddress = true) => {
+    cy.get('#firstName').type(customerData.shippingInfo.firstName);
+    cy.get('#lastName').type(customerData.shippingInfo.lastName);
+    cy.get('#address1').type(customerData.shippingInfo.address1);
+    cy.get('#address2').type(customerData.shippingInfo.address2);
+    cy.get('#city').type(customerData.shippingInfo.city);
+    cy.get('#zip').type(customerData.shippingInfo.zip);
+    cy.get('#state').type(customerData.shippingInfo.state);
+    !fail && cy.get('#country').type(customerData.shippingInfo.country);
+    saveAddress && cy.get('.PrivateSwitchBase-input-10').click();
 });
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('fillPayment', (customerData, fail = false, saveInformation = true) => {
+    cy.get('#cardName').type(customerData.paymentInfo.cardName);
+    cy.get('#cardNumber').type(customerData.paymentInfo.cardNumber);
+    cy.get('#expDate').type(customerData.paymentInfo.expDate);
+    !fail && cy.get('#cvv').type(customerData.paymentInfo.cvv);
+    saveInformation && cy.get('.PrivateSwitchBase-input-10').click();
+});
