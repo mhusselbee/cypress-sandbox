b1afb98b616f806d81b8407323f99b8b0828f6ed
Mateo Husselbee <mhusselbee@cbtnuggets.com>
2021-03-26 16:10:05 -0700

    Fix a bunch of stuff

--- cypress-frontend/src/lib/state/useShoppingCart.tsx
+++ cypress-frontend/src/lib/state/useShoppingCart.tsx
@@ -1,43 +1,48 @@
-import { useState } from 'react';
-import { createContainer } from 'unstated-next';
+import { useState } from "react";
+import { createContainer } from "unstated-next";
 
-const productsFixture = [{ name: 'Vegetable', price: 1, quantity: 0, sku: '11111', details: {} }];
+const productsFixture = [
+  { name: "Vegetable", price: 1, quantity: 0, sku: "11111", details: {} },
+];
 
 const useCartContainer = () => {
-    const [products, setProducts] = useState<Product[]>(productsFixture);
+  const [products, setProducts] = useState<Product[]>(productsFixture);
 
-    const addOne = (sku: string) => {
-        setProducts(prev => {
-            const index = prev.findIndex(p => p.sku === sku);
-            if (index > 0) {
-                console.log(true);
-            }
-            return prev;
-        });
-        console.log('product added');
-        return products.length;
-    };
+  const addOne = (sku: string) => {
+    setProducts((prev) => {
+      const index = prev.findIndex((p) => p.sku === sku);
+      if (index > 0) {
+        console.log(true);
+      }
+      return prev;
+    });
+    console.log("product added");
+    return products.length;
+  };
 
-    const remove = (productSku: string) => {
-        setProducts(prev => {
-            return prev.filter(product => product.sku !== productSku);
-        });
-        console.log('product removed');
-        return products.length;
-    };
+  const remove = (productSku: string) => {
+    setProducts((prev) => {
+      return prev.filter((product) => product.sku !== productSku);
+    });
+    console.log("product removed");
+    return products.length;
+  };
 
-    const calculateSubTotal = () => {
-        return products.reduce((prev, pro) => {
-            return prev + pro.price;
-        }, 0);
-    };
+  const calculateSubTotal = () => {
+    return products.reduce((prev, pro) => {
+      return prev + pro.price;
+    }, 0);
+  };
 
-    return {
-        products,
-        add: addOne,
-        remove,
-        calculateSubTotal,
-    };
+  return {
+    products,
+    add: addOne,
+    remove,
+    calculateSubTotal,
+  };
 };
 
-export const { Provider: CartProvider, useContainer: useShoppingCart } = createContainer(useCartContainer);
+export const {
+  Provider: CartProvider,
+  useContainer: useShoppingCart,
+} = createContainer(useCartContainer);
--- cypress-runner/cypress/integration/signin.ts
+++ cypress-runner/cypress/integration/signin.ts
@@ -1,6 +1,6 @@
 describe(`A customer in the ${Cypress.env('environment')} environment`, () => {
     it(' can sign in', () => {
-        cy.visit('http://localhost:3001');
+        cy.visit('http://localhost:3000');
         //@ts-ignore
         cy.signIn();
     });
